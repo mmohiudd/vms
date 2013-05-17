b2e636ede541933f5ea78b0835defe0a3df3cd1e
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2013-05-17 09:46:28 -0400

    remobe host entries before adding it to make sure there are no duplicates

--- provision/shell/network.sh
+++ provision/shell/network.sh
@@ -3,7 +3,14 @@ rm -f /etc/udev/rules.d/70-persistent-net.rules
 rm -f /etc/sysconfig/network-scripts/ifcfg-eth1
 /etc/init.d/network restart
 
-# write to hosts 
+# remove existig entry, if any
+sed -i '/'master1.localhost'$/ d' /etc/hosts
+# add a new one
 echo "192.168.100.22	master1.localhost" >> /etc/hosts
+
+
+sed -i '/'slave1.localhost'$/ d' /etc/hosts
 echo "192.168.100.23	slave1.localhost" >> /etc/hosts
+
+sed -i '/'slave2.localhost'$/ d' /etc/hosts
 echo "192.168.100.24	slave2.localhost" >> /etc/hosts
