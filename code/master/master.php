7a469fb129bdd546bd158810a543510187344f2b
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2013-05-17 02:46:39 -0400

    adding master and slave code

--- code/master/master.php
+++ code/master/master.php
@@ -4,10 +4,9 @@ error_reporting(E_ALL);
 date_default_timezone_set("America/Toronto"); 
 
 set_time_limit(0); // do not timeout
-$sock = socket_create(AF_INET, SOCK_STREAM, 0);
 
 $hostname = gethostname(); 
-$address = gethostbyname($hostname);
+$address = "192.168.100.22";
 $port = 5000;
 
 _log("started master daemon " . $hostname);
@@ -27,31 +26,79 @@ if( !socket_bind($sock, $address , $port) ){
 	exit(1);
 }
 
-socket_listen($sock);
-
-$loop = 1;
-
-while($loop == 1){
-
-	$client = socket_accept($sock);
-	$input = socket_read($client, 1024);
-	echo $input . "\n\n";
-
-	if ($input == 'exit') {
-		$close = socket_close($sock);
-		$loop = 0;
-	}
-
-	if($loop == 1) {
-		_log($input);
-	}
-
+// Bind the source address
+if( !socket_listen($sock, 5) ){	
+	$errorcode = socket_last_error();
+	$errormsg = socket_strerror($errorcode);
+	_log("[error] Couldn't listen to socket: [$errorcode] $errormsg.");
+	exit(1);
 }
 
+//clients array
+$clients = array();
 
-
-
-
+do {
+    $read = array();
+    $read[] = $sock;
+    
+    $read = array_merge($read,$clients);
+    
+    // Set up a blocking call to socket_select
+    if(socket_select($read, $write, $except,5) < 1)
+    {
+        //    SocketServer::debug("Problem blocking socket_select?");
+        echo ".";
+        continue;
+    }
+    
+    // Handle new Connections
+    if (in_array($sock, $read)) {        
+        
+        if (($msgsock = socket_accept($sock)) === false) {
+        	$errorcode = socket_last_error();
+			$errormsg = socket_strerror($errorcode);
+			_log("[error] Couldn't accept socket: [$errorcode] $errormsg.");
+            break;
+        }
+        $clients[] = $msgsock;
+        $key = array_keys($clients, $msgsock);
+        
+        $msg = "\nWelcome to the PHP Test Server. \n" .
+        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
+        socket_write($msgsock, $msg, strlen($msg));
+        
+    }
+    
+    // Handle Input
+    foreach ($clients as $key => $client) { // for each client        
+        if (in_array($client, $read)) {
+        	
+            if (false === ($buf = socket_read($client, 2048, PHP_NORMAL_READ))) {
+            	$errorcode = socket_last_error();
+				$errormsg = socket_strerror($errorcode);
+                _log("[error] Couldn't read from socket: [$errorcode] $errormsg.");
+                break 2;
+            }
+            if (!$buf = trim($buf)) {
+                continue;
+            }
+            if ($buf == 'quit') {
+                unset($clients[$key]);
+                socket_close($client);
+                break;
+            }
+            if ($buf == 'shutdown') {
+                socket_close($client);
+                break 2;
+            }
+            
+            $talkback = "Cliente {$key}: '$buf'.\n";
+            socket_write($client, $talkback, strlen($talkback));
+            _log($buf);
+        }
+        
+    }        
+} while (true);
 
 socket_close($sock); // close this socket
 
--- code/slave/slave.php
+++ code/slave/slave.php
@@ -1,49 +1,8 @@
 <?php
 error_reporting(E_ALL);
 date_default_timezone_set("America/Toronto"); 
-/*
-// Get the port for the WWW service.
-$service_port = getservbyname('www', 'tcp');
-
-// Get the IP address for the target host.
-$address = gethostbyname('www.example.com');
-
-// Create a TCP/IP socket. 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if ($socket === false) {
-    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-} else {
-    echo "OK.\n";
-}
-
-echo "Attempting to connect to '$address' on port '$service_port'...";
-$result = socket_connect($socket, $address, $service_port);
-if ($result === false) {
-    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-} else {
-    echo "OK.\n";
-}
-
-$in = "HEAD / HTTP/1.1\r\n";
-$in .= "Host: www.example.com\r\n";
-$in .= "Connection: Close\r\n\r\n";
-$out = '';
-
-echo "Sending HTTP HEAD request...";
-socket_write($socket, $in, strlen($in));
-echo "OK.\n";
-
-echo "Reading response:\n\n";
-while ($out = socket_read($socket, 2048)) {
-    echo $out;
-}
-
-echo "Closing socket...";
-socket_close($socket);
-echo "OK.\n\n";
-
-*/
 set_time_limit(0); // do not timeout
+
 $sock = socket_create(AF_INET, SOCK_STREAM, 0);
 
 $hostname = "master1.localhost";
@@ -52,44 +11,38 @@ $port = 5000;
 
 _log("started slave daemon " . $hostname);
 
-if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) {
-	$errorcode = socket_last_error();
-	$errormsg = socket_strerror($errorcode);
-	_log("[error] Couldn't create socket: [$errorcode] $errormsg.");
-	exit(1);
-}
 
-_log(sprintf("connect to %s %s:%s", $hostname, $address, $port));
-
-// Bind the source address
-if( !socket_bind($sock, $address , $port) ){	
-	$errorcode = socket_last_error();
-	$errormsg = socket_strerror($errorcode);
-	_log("[error] Couldn't bind socket: [$errorcode] $errormsg.");
-	exit(1);
-}
-
-socket_listen($sock);
-
-while($con == 1){
-	socket_write($socket, $in, strlen($in));
-
-	$client = socket_accept($sock);
-	$input = socket_read($client, 1024);
-
-	if($con == 1) {
-		_log($input);
+do{
+	$connected = True;
+	if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) {
+		$errorcode = socket_last_error();
+		$errormsg = socket_strerror($errorcode);
+		_log("[error] Couldn't create socket: [$errorcode] $errormsg.");
+		$connected = False;
+		
 	}
 
-	sleep(2); // sleep for 2 seconds
-}
+	// Bind the source address
+	if( !socket_connect($sock, $address , $port) ){	
+		$errorcode = socket_last_error();
+		$errormsg = socket_strerror($errorcode);
+		_log("[error] Couldn't connect to socket: [$errorcode] $errormsg.");
+		$connected = False;
+	}
+	
+	if($connected){
+		$message = "Hello from " . gethostname();
+		socket_write($sock, $message, strlen($message));
+		_log("connected to " . $hostname);
+	}
+
+	sleep(5); // sleep for 5 seconds
+}while(True);
 
 
 
 
 
-
-socket_close($socket); // close this socket
 
 /**
 * write whatever is in the $log. 
